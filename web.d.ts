dd718b7da6b1d8a8088bbf5ad5397889db71c43a
nin-jin <nin-jin@users.noreply.github.com>
2023-01-22 07:08:17 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -645,6 +645,7 @@ declare namespace $ {
         };
         adopt(transfer?: any): {};
         receive(transfer?: any): any;
+        allow(): readonly any[];
         enter(event?: any): any;
         move(event?: any): any;
         leave(event?: any): any;
@@ -659,6 +660,7 @@ declare namespace $.$$ {
         protected _target: EventTarget | null;
         enter(event: DragEvent): void;
         move(event: DragEvent): void;
+        decide_action(event: DragEvent): any;
         leave(event: DragEvent): void;
         receive(transfer: unknown): unknown;
         drop(event: DragEvent): void;
